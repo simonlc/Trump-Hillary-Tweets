5350da9ddba4b2b3340d8412b40658375dfdb474
Simon Laroche <hi@simon.lc>
2016-11-11 20:07:00 -0500

    Change port

--- app/server/index.js
+++ app/server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3000;
 
 /**
  * Setup tweet stream
